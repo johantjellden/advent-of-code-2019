5ee1fa69c2bf6dff80611208aae5141b52618ab3
Johan Tjelldén <johan@hedvig.com>
2019-12-02 16:30:07 +0100

    Day one part two

--- one.py
+++ one.py
@@ -11,14 +11,33 @@ class MassCalculator:
 
     @staticmethod
     def calculate_total_fuel_requirements(mass_inputs):
-        return sum([MassCalculator.calculate_fuel(int(x)) for x in mass_inputs])
+        return sum([MassCalculator.calculate_fuel_with_own_mass(int(x)) for x in mass_inputs])
 
     @staticmethod
     def calculate_fuel(mass):
         return floor(mass / 3) - 2
+    
+    @staticmethod
+    def calculate_fuel_with_own_mass(mass):
+        if mass == 0:
+            return 0
+        fuel = max(MassCalculator.calculate_fuel(mass), 0)
+        return fuel + MassCalculator.calculate_fuel_with_own_mass(fuel)
 
 
 class TestOne(unittest.TestCase):
+
+    def test_calculate_fuel_with_own_mass_12(self):
+        fuel = MassCalculator.calculate_fuel_with_own_mass(12)
+        self.assertEqual(fuel, 2)
+
+    def test_calculate_fuel_with_own_mass_1969(self):
+        fuel = MassCalculator.calculate_fuel_with_own_mass(1969)
+        self.assertEqual(fuel, 966)
+
+    def test_calculate_fuel_with_own_mass_100756(self):
+        fuel = MassCalculator.calculate_fuel_with_own_mass(100756)
+        self.assertEqual(fuel, 50346)
 
     def test_calculate_12(self):
         fuel = MassCalculator.calculate_fuel(12)
